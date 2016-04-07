e99c15f9bed2dba007c9b52a4a795c33c94f0e0a
Deividas Eringis <eringis@gmail.com>
2016-04-07 17:02:34 +0300

    Added slider and other elements

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,25 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func showAlert(){
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first app", preferredStyle: .Alert)
-        
-        let action = UIAlertAction(title: "Awesome", style: .Default, handler: nil)
-        
-        alert.addAction(action)
-        
-        presentViewController(alert, animated: true, completion: nil)
-    }
+  var currentValue: Int = 50
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func showAlert(){
+    let alertMessage = "The value of the slider is: \(currentValue)"
+    
+    let alert = UIAlertController(title: "Hello world", message: alertMessage, preferredStyle: .Alert)
+    
+    let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+    
+    alert.addAction(action)
+    
+    presentViewController(alert, animated: true, completion: nil)
+  }
+  
+  @IBAction func sliderMoved(slider: UISlider){
+    currentValue = lroundf(slider.value)
+  }
 }
 
