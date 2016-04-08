9ee61709ae2c42ab3f538363f9db29d062aa8edc
Deividas Eringis <eringis@gmail.com>
2016-04-08 10:40:02 +0300

    Added random number generator

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,9 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-  var currentValue: Int = 50
+  var currentValue: Int = 0
+  @IBOutlet weak var slider: UISlider!
+  var targetValue: Int = 0
+
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    currentValue = lroundf(slider.value)
+    targetValue = 1 + Int(arc4random_uniform(100))
+    
     // Do any additional setup after loading the view, typically from a nib.
   }
   
@@ -21,7 +28,7 @@ class ViewController: UIViewController {
   }
   
   @IBAction func showAlert(){
-    let alertMessage = "The value of the slider is: \(currentValue)"
+    let alertMessage = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
     
     let alert = UIAlertController(title: "Hello world", message: alertMessage, preferredStyle: .Alert)
     
@@ -33,7 +40,7 @@ class ViewController: UIViewController {
   }
   
   @IBAction func sliderMoved(slider: UISlider){
-    currentValue = lroundf(slider.value)
+    
   }
 }
 
