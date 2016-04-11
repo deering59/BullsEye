77211ce0a6289504d8663661004aa78d47f0e5dd
Deividas Eringis <eringis@gmail.com>
2016-04-11 16:58:35 +0300

    Added Crossfade animations on pressing reset button

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
   var currentValue: Int = 0
@@ -114,6 +115,14 @@ class ViewController: UIViewController {
   @IBAction func startOver() {
     startNewGame()
     updateLabels()
+    
+    let transition = CATransition()
+    transition.type = kCATransitionFade
+    transition.duration = 1
+    transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+    view.layer.addAnimation(transition, forKey: nil)
+    
+    
   }
   
 }
